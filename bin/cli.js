e71c7fe92a7ccf5b28161f99907f197563ae64ec
Andrew Knotts <knottsan2010@gmail.com>
2022-11-23 21:58:00 -0500

    finished function

---
+++ bin/cli.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import minimist from "minimist"
+import { roll } from "/lib/roll.js"
+
+//reads in the command
+const args = minimist(proccess.argv.slice(2));
+
+//default values to be set if not specified
+const sides = args.sides || 6;
+const dice = args.dice || 2;
+const rolls = args.rolls || 1;
+
+//return the result of the function
+console.log(JSON.stringify(roll(sides, dice, rolls)))
